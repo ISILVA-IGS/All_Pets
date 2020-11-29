8943bbfa6127b960016cf9cf47d7a7fe60586e47
Igor Santos <38841979+ISILVA-IGS@users.noreply.github.com>
2020-11-28 23:30:08 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ set -ex
 
 sudo kill -9 $(sudo lsof -t -i:8081) &&
 sudo cp *.jar / &&
+echo ok &&
 sudo /usr/lib/jvm/java-14-openjdk-amd64/bin/java -jar /jenkins/AllPets-0.0.1-SNAPSHOT.jar --server.port=8081&
+echo start
